06b3981d203b170f7dbce7ac71a3d035f8728a3f
mansfire <44177254+mansfire@users.noreply.github.com>
2024-07-16 20:40:15 -0600

    Update Streamlit_demo_apss.py

--- Streamlit_demo_apss.py
+++ Streamlit_demo_apss.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+[theme]
+primaryColor="#FF4B4B"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#31333F"
 st.header('st.button')
 
 if st.button('Say hello'):
